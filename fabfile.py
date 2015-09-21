e0db4eb399320ca0884c7ff3f79d5f19079405f9
Nikita Marchenko <nmarchenko@mirantis.com>
2015-09-21 14:19:57 +0300

    add htop

--- fabfile.py
+++ fabfile.py
@@ -10,6 +10,6 @@ env.hosts = ['root@{}'.format(FUEL_VM)]
 def init():
     local('ssh-keygen -f "/home/nmarchenko/.ssh/known_hosts" -R {}'.format(FUEL_VM))
     local('ssh-copy-id -i ~/.ssh/id_rsa.pub root@{}'.format(FUEL_VM))
-    run('yum install nano -y')
+    run('yum install nano htop -y')
     run('mv /etc/fuel/client/config.yaml ~/.config.yaml')
     run('echo "export FUELCLIENT_CUSTOM_SETTINGS=/root/.config.yaml" >> .bashrc')
