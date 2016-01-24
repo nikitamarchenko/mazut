c7669bcdb32942facc5ff184d5864f4be61ba8bc
Nikita Marchenko <nmarchenko@mirantis.com>
2016-01-24 03:36:34 +0200

    add additional check for remove host

--- fabfile.py
+++ fabfile.py
@@ -9,6 +9,7 @@ env.hosts = ['root@{}'.format(FUEL_VM)]
 
 def init():
     local('ssh-keygen -f "/home/nmarchenko/.ssh/known_hosts" -R {}'.format(FUEL_VM))
+    local('ssh-keygen -f "/home/nmarchenko/.ssh/known_hosts" -R 10.20.0.2')
     local('ssh-copy-id -i ~/.ssh/id_rsa.pub root@{}'.format(FUEL_VM))
     run('yum install nano htop -y')
     run('mv /etc/fuel/client/config.yaml ~/.config.yaml')
