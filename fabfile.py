196cd8f291544893a9005fb565a25ead64e5714d
Nikita Marchenko <nikita.marchenko@gmail.com>
2016-01-27 12:14:00 -0600

    using ~ for resolving home directory

--- fabfile.py
+++ fabfile.py
@@ -8,8 +8,8 @@ env.hosts = ['root@{}'.format(FUEL_VM)]
 
 
 def init():
-    local('ssh-keygen -f "/home/nmarchenko/.ssh/known_hosts" -R {}'.format(FUEL_VM))
-    local('ssh-keygen -f "/home/nmarchenko/.ssh/known_hosts" -R 10.20.0.2')
+    local('ssh-keygen -f ~/.ssh/known_hosts -R {}'.format(FUEL_VM))
+    local('ssh-keygen -f ~/.ssh/known_hosts -R 10.21.0.2')
     local('ssh-copy-id -i ~/.ssh/id_rsa.pub root@{}'.format(FUEL_VM))
     run('yum install nano htop -y')
     run('mv /etc/fuel/client/config.yaml ~/.config.yaml')
